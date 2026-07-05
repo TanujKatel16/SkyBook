4d57858c8cf0425a60b1562720992faa86b58e0a
Tanujkatel16 <tanujkatel9@gmail.com>
2026-07-06 01:41:46 +0530

    Setup Express application and middleware

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
-import express from "express"
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import healthRouter from "./routes/health.routes.js";
 
 const app=express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true,
+}));
+app.use(cookieParser());
+app.use("/api/v1/health",healthRouter);
 
 
 export {app};
